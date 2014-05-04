0c6e6407ae43813b640e26749ee2d79cb489394d
Lorenzo Lopez <lorenzo.lopez@intec.es>
2014-05-04 07:11:35 +0000

    Added basic tests for parse_args Added parse_args

--- test/issues_test.exs
+++ test/issues_test.exs
@@ -1,7 +1,18 @@
 defmodule IssuesTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  import Issues.CLI, only: [parse_args: 1]
+
+  test ":help returned by option parsing with --help or -h" do
+    assert parse_args(["-h", "anything"]) == :help
+    assert parse_args(["--help", "anything"]) == :help
+  end
+
+  test "Three values returned if tree given" do
+    assert parse_args(["user","project","99"]) == { "user", "project", 99 }
+  end
+
+  test "count is defaulted if 2 values given" do
+    assert parse_args(["user","project"]) == { "user", "project", 4 }
   end
 end
